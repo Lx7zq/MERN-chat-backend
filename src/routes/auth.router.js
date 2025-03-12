ee40ddbfa724a031a09dba33c7a6a0ff5ca27f01
Lx7zq <thisforworkonly7@gmail.com>
2025-03-12 17:19:11 +0700

    signup signin signout updatepic

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { signup, signin, signout } = require("../controllers/auth.controller");
+const {
+  signup,
+  signin,
+  signout,
+  uploadProfilePic,
+} = require("../controllers/auth.controller");
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/signout", signout);
+router.put("/update-profile", uploadProfilePic);
 
 module.exports = router;
